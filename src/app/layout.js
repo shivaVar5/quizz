f16f79974a7086f9e027e12ba46c6ba99decc95a
Shiva <shiva.varikuppala@veniso.com>
2025-02-27 12:56:06 +0530

    Initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,32 +1,34 @@
-import React from "react";
-import "./_styles/globals.css"
+"use client"; 
+import React, { Suspense } from "react";
+import "./_styles/globals.css";
+import img from "../public/bg.jpg"
 
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body className="flex flex-col bg-primary-950 text-primary-100 min-h-screen gap-1">
+        <main className="flex-grow">
+          <div
+            style={{
+              backgroundImage: "url('/bg.jpg')",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              minHeight: "100vh",
+            }}
+          >
+            <Suspense fallback={<div>Loading...</div>}>
+              {children}  
+            </Suspense>
+          </div>
+        </main>
 
-const RootLayout = ({children})=>{
-    return(
-        <html lang="en">
-            <body className="flex flex-col bg-primary-950 text-primary-100 min-h-screen gap-1">
-               
-                <main className="flex-grow ">
-                    <div  style={{
-                backgroundImage: "url('/bg.jpg')",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                minHeight: "100vh",
-              }}
-               >
-                   {children}  
-                </div> 
-                </main>
-               
-               <footer className="bg-gray-500 flex justify-evenly p-4 font-semibold font-sans">
-                <p>Designed and developed by @shivavar</p>
-                <p className="">2025 Quiz competetion copyright</p>
-               </footer>
-               
+        <footer className="bg-accent-500 flex justify-evenly p-4 font-semibold font-sans">
+          <p>Designed and developed by @shivavar</p>
+          <p>2025 Quiz competition &copy;</p>
+        </footer>
+      </body>
+    </html>
+  );
+};
 
-            </body>
-        </html>
-    )
-}
 export default RootLayout;
